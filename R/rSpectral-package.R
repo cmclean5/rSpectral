15adfd40cd59d71248d278d5f7f65ceb48da65ad
Anatoly Sorokin <lptolik@gmail.com>
2022-07-08 18:06:06 +0100

    package level documentation

---
+++ R/rSpectral-package.R
@@ -0,0 +1,15 @@
+#' rSpectral
+#' 
+#' This package implements Spectral Modularity clustering algorithm for
+#' \code{igraph} and \code{graphNEL} graphs. 
+#' 
+#' @docType package
+#' @encoding UTF-8
+#' @author Colin Mclean <Colin.D.Mclean@ed.ac.uk>
+#' @importFrom Rcpp evalCpp
+#' @useDynLib rSpectral
+#' @name rSpectral
+#' 
+"_PACKAGE"
+
+NULL

813245b9d75df08b67a94c32b29806cb62204308
Anatoly Sorokin <lptolik@gmail.com>
2022-07-14 12:40:03 +0100

    fix igraph functions

--- R/igraph.R
+++ R/igraph.R
@@ -13,8 +13,8 @@ spectral_igraph_membership<-function(g,Cn_min = 1L, tol = 0.00001, names = 1L, f
   if(!inherits(g,'igraph')){
     stop('Graph should be "igraph" object.')
   }
-  el = as.data.frame(igraph::get.edgelist(gg,names=T))
-  rSpectral::load_data(df=df)
+  el = as.data.frame(igraph::get.edgelist(g,names=T))
+  rSpectral::load_data(df=el)
   status = rSpectral::spectral(fix_neig=1)
   spec   = rSpectral::membership(detach_graph=1)
   spec.df<- data.frame(names=spec$ID,membership=spec$K)
@@ -25,7 +25,7 @@ spectral_igraph_membership<-function(g,Cn_min = 1L, tol = 0.00001, names = 1L, f
 #'
 #' @param g \code{igraph} object
 #' @inheritParams spectral
-#' @return \code{communitios} object
+#' @return \code{communities} object
 #' @export
 #' @importFrom igraph modularity vcount
 #'
@@ -41,7 +41,8 @@ spectral_igraph_communities<-function(g,Cn_min = 1L, tol = 0.00001, names = 1L, 
   res$vcount <- igraph::vcount(g)
   res$algorithm <- "spectral"
   res$membership <- df.mem$membership
-  res$modularity <- igraph::modularity(karate,c$membership)
+  res$modularity <- igraph::modularity(g,df.mem$membership)
   res$names <- df.mem$names
   class(res) <- "communities"
+  return(res)
 }

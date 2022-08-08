878fc0b15eca2305bbdc3ab8357ced2d760dc366
Anatoly Sorokin <lptolik@gmail.com>
2022-08-08 12:27:03 +0900

    formatting clearance

--- R/graphNEL.R
+++ R/graphNEL.R
@@ -16,16 +16,18 @@
 #' g2 = randomEGraph(V, edges=20)
 #' mem.df = spectral_graphNEL(g2)
 #' head(mem.df)
-spectral_graphNEL<-function(g,Cn_min = 1L, tol = 0.00001, names = 1L, fix_neig = 0L){
+spectral_graphNEL <- function(g, Cn_min = 1L, tol = 0.00001, names = 1L,
+                              fix_neig = 0L){
   if(!inherits(g,'graphNEL')){
     stop('Graph should be "graphNEL" object.')
   }
-  led<-graph::edges(g)
-l<-lapply(names(led),function(.x)data.frame(V1=rep(.x,length(led[[.x]])),V2=led[[.x]]))
-df<-do.call(rbind,l)
-load_data(df=df)
-status = spectral(fix_neig=1)
-spec   = membership(detach_graph=1)
-spec.df<- data.frame(names=spec$ID,membership=spec$K)
+  led <- graph::edges(g)
+l <- lapply(names(led),function(.x)data.frame(V1=rep(.x,length(led[[.x]])),
+                                              V2=led[[.x]]))
+df <- do.call(rbind,l)
+load_data(df = df)
+status <- spectral(fix_neig=1)
+spec   <- membership(detach_graph=1)
+spec.df <- data.frame(names=spec$ID,membership=spec$K)
 return(spec.df)
 }
